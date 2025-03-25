6783b2d2d660ee73271e7e6af60e37749b580ad3
Vaishak Menon <vaishakkmenon@gmail.com>
2025-03-25 18:34:59 +0000

    missing import

--- tokenization/custom_tokenizer/__init__.py
+++ tokenization/custom_tokenizer/__init__.py
@@ -1,4 +1,4 @@
-from .config import DATA_PATH, TOKENIZER_PATH, VOCAB_SIZE, MIN_FREQUENCY, SPECIAL_TOKENS
+from .config import DATA_PATH, TOKENIZER_PATH, VOCAB_SIZE, MIN_FREQUENCY, SPECIAL_TOKENS, NUM_CORES
 from .data_processing import load_dataset, clean_and_save_dataset
 from .trainer import load_tokenizer
 
@@ -8,6 +8,7 @@ __all__ = [
     "VOCAB_SIZE",
     "MIN_FREQUENCY",
     "SPECIAL_TOKENS",
+    "NUM_CORES",
     "load_tokenizer",
     "load_dataset",
     "clean_and_save_dataset"
