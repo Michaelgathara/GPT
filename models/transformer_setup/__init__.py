e31fabfb68d8940f248f954e1459943ae73d4c9a
Michaelgathara <mikegtrm@gmail.com>
2025-04-03 14:00:49 -0500

    lord help us

--- models/transformer_setup/__init__.py
+++ models/transformer_setup/__init__.py
@@ -1,4 +1,4 @@
 from .params import ModelConfig
-from .transformer import MultiHead, Head, FeedForward, Block, TransformerModel
+from .transformer import Head, FeedForward, Block, TransformerModel
 
-__all__ = ['ModelConfig', 'MultiHead', 'Head', 'FeedForward', 'Block', 'TransformerModel']
+__all__ = ['ModelConfig', 'Head', 'FeedForward', 'Block', 'TransformerModel']
