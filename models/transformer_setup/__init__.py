1aa83f01cfc479e3b69d314db86cf5987a23ee8d
akumaluffy <yliu0602@gmail.com>
2025-04-03 22:27:05 -0500

    copy rest of transformer and custom_BPE to new files to work with for mla

--- models/transformer_setup/__init__.py
+++ models/transformer_setup/__init__.py
@@ -1,4 +1,6 @@
 from .params import ModelConfig
 from .transformer import FlashAttentionHead, MultiHead, Head, FeedForward, Block, TransformerModel
 
+from .mla import FlashAttentionHead, MultiHead, Head, FeedForward, Block, TransformerModel
+
 __all__ = ['ModelConfig', 'FlashAttentionHead', 'MultiHead', 'Head', 'FeedForward', 'Block', 'TransformerModel']
