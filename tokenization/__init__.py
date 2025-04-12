ebe3926aef0ed5ccdf1c7737fb9aa1de1958bb3f
michaelgathara <mikegtrm@gmail.com>
2025-04-12 01:20:49 -0500

    new tokenization init

--- tokenization/__init__.py
+++ tokenization/__init__.py
@@ -1,10 +1,23 @@
 from .custom_tokenizer import (
-    config,
-    train_tokenizer,
-    data_processing,
-    trainer
+    DATA_PATH,
+    TOKENIZER_PATH,
+    VOCAB_SIZE,
+    MIN_FREQUENCY,
+    SPECIAL_TOKENS,
+    NUM_CORES,
+    load_tokenizer,
+    load_dataset,
+    clean_and_save_dataset
 )
 
-from .tiktoken_tokenizer import get_tiktoken_tokenizer
-
-__all__ = ['get_tiktoken_tokenizer', "config", "train_tokenizer", "data_processing", "trainer"]
+__all__ = [
+    "DATA_PATH",
+    "TOKENIZER_PATH",
+    "VOCAB_SIZE",
+    "MIN_FREQUENCY",
+    "SPECIAL_TOKENS",
+    "NUM_CORES",
+    "load_tokenizer",
+    "load_dataset",
+    "clean_and_save_dataset"
+]
