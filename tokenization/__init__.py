1c36265637d881989eb0fab7047cd5cbd41ded27
michaelgathara <mikegtrm@gmail.com>
2025-04-14 21:35:31 -0500

    imports

--- tokenization/__init__.py
+++ tokenization/__init__.py
@@ -1,13 +1,10 @@
 from .custom_tokenizer import (
     DATA_PATH,
-    TOKENIZER_PATH,
+    FINEWEB_TOKENIZER_PATH,
     VOCAB_SIZE,
     MIN_FREQUENCY,
     SPECIAL_TOKENS,
-    NUM_CORES,
-    load_tokenizer,
-    load_dataset,
-    clean_and_save_dataset
+    NUM_CORES
 )
 
 from .train_fineweb_tokenizer import train_fineweb_tokenizer, fine_web_iterator
@@ -15,14 +12,11 @@ from .utils import load_fineweb_tokenizer
 
 __all__ = [
     "DATA_PATH",
-    "TOKENIZER_PATH",
+    "FINEWEB_TOKENIZER_PATH",
     "VOCAB_SIZE",
     "MIN_FREQUENCY",
     "SPECIAL_TOKENS",
     "NUM_CORES",
-    "load_tokenizer",
-    "load_dataset",
-    "clean_and_save_dataset",
     "train_fineweb_tokenizer",
     "fine_web_iterator",
     "load_fineweb_tokenizer"
--- tokenization/custom_tokenizer/config.py
+++ tokenization/custom_tokenizer/config.py
@@ -5,7 +5,7 @@ BASE_FOLDER = os.path.abspath("../..")
 sys.path.append(BASE_FOLDER)
 
 DATA_PATH = f"{BASE_FOLDER}/tokenization/fineweb_tokenizer.txt"
-INEWEB_TOKENIZER_PATH = f"{BASE_FOLDER}/tokenization/fineweb_tokenizer.json"
+FINEWEB_TOKENIZER_PATH = f"{BASE_FOLDER}/tokenization/fineweb_tokenizer.json"
 
 NUM_CORES = max(1, multiprocessing.cpu_count()) - 1
 
