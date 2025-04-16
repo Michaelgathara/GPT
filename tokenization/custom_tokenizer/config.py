303d4fdfbb7bfaa2185d3a2395ef30ceef772372
michaelgathara <mikegtrm@gmail.com>
2025-04-16 01:11:15 -0500

    base folder

--- tokenization/custom_tokenizer/config.py
+++ tokenization/custom_tokenizer/config.py
@@ -1,7 +1,7 @@
 import os, sys
 import multiprocessing
 
-BASE_FOLDER = os.path.abspath("../..")
+BASE_FOLDER = os.path.abspath("../")
 sys.path.append(BASE_FOLDER)
 
 DATA_PATH = f"{BASE_FOLDER}/tokenization/fineweb_tokenizer.txt"
