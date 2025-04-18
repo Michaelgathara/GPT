ae42e5b440479a7dfb8dd0975cabc9a862822599
Elisabeth Molen <emolen@uab.edu>
2025-04-18 10:11:12 -0500

    hyper_param_tuning plan

---
+++ models/transformer_setup/params_hyper_tuning.py
@@ -0,0 +1,24 @@
+'''
+Hyperparameter Tuning Plan
+
+Below are some suggested values for tuning the model hyperparameters.  
+
+Full plan to come ....
+
+'''
+
+# Suggested Hyperparameter values for tuning
+
+# Model Architecture Hyperparameters
+
+layer_vals = [8, 10, 12, 14, 16] # Probably won't do all - likely pick 3 or 4 values
+
+attention_head_vals = [8, 10, 12, 14, 16] # Probably won't do all - likely pick 3 or 4 values
+
+# Training Hyperparameters
+
+learning_rate_vals = [1e-3,]
+
+# Optimizer Setting Hyperparameters
+
+weight_decay_vals = []
