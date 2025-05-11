6f7fddaf4382595d2e787d58e1098b202db2ce51
michaelgathara <mikegtrm@gmail.com>
2025-05-11 16:51:11 -0500

    chore: cleaned up transformer_setup init

--- models/transformer_setup/__init__.py
+++ models/transformer_setup/__init__.py
@@ -1,8 +1,4 @@
 from .params import ModelConfig
 
-# from .mla_transformer import LatentAttentionHead, MultiHeadLatentAttention, FeedForward, Block, TransformerModel 
-
-# __all__ = ['ModelConfig', 'LatentAttentionHead', 'MultiHeadLatentAttention', 'FeedForward', 'Block', 'TransformerModel']
-
 from .transformer import TransformerModel, FeedForward, Block
 __all__ = ['ModelConfig', 'TransformerModel', 'FeedForward', 'Block']
